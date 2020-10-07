74b0da593cfc138e9bc8a06d80c4cc7fcd0c7059
John <jortega43@ucmerced.edu>
2020-10-07 14:32:37 -0700

    1st mongo commit

--- server/server.js
+++ server/server.js
@@ -1,3 +1,30 @@
+const {MongoClient} = require('mongodb');
+
+async function main(){
+  const url = "mongodb+srv://jortega43:Shinobu_101@cluster0.qq28k.mongodb.net/Cluster0?retryWrites=true&w=majority";
+  const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+
+  try{
+    await client.connect();
+    await listDatabases(client);
+  }
+  catch(e){
+    console.error(e);
+  }
+  finally{
+    await client.close();
+  }
+}
+
+main().catch(console.error);
+
+async function listDatabases(client){
+  databasesList = await client.db().admin().listDatabases();
+
+  console.log("Databases");
+  databasesList.databases.forEach(db => console.log(` -${db.name}`));
+}
+
 const LEF = [
     [3, 4, 5, 5, 5],
     [2, 3, 4, 4, 4],
