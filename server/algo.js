d36744841c3a9e1609f64a4fcd4f5fd1d4734560
Burritoj <matsimoncode@gmail.com>
2020-10-09 22:59:00 -0700

    algo.js and branch

---
+++ server/algo.js
@@ -0,0 +1,52 @@
+//verylow=0, low=1, medium=2, high=3, veryhigh=4
+//flipped horizontally from excel tool's derivation page for matrix indecies
+M_PLEF = [
+    [0, 0, 0, 0, 0],
+    [0, 0, 1, 1, 1],
+    [0, 1, 2, 2, 2],
+    [1, 2, 3, 3, 3],
+    [2, 3, 4, 4, 4]
+  ];
+const M_VULN = [
+    [2, 1, 0, 0, 0],
+    [3, 2, 1, 0, 0],
+    [4, 3, 2, 1, 0],
+    [4, 4, 3, 2, 1],
+    [4, 4, 4, 3, 2]
+  ];
+const M_SLEF = [
+    [0, 0, 0, 1, 2],
+    [0, 0, 1, 2, 3],
+    [0, 1, 2, 3, 4],
+    [1, 2, 3, 4, 4],
+    [2, 3, 4, 4, 4]
+  ];
+const M_primaryRisk = [
+    [0, 0, 0, 1, 2],
+    [0, 0, 1, 2, 3],
+    [0, 1, 2, 3, 4],
+    [1, 2, 3, 4, 4],
+    [2, 3, 4, 4, 4]
+  ];
+const M_secondaryRisk = [
+    [0, 0, 0, 1, 2],
+    [0, 0, 1, 2, 3],
+    [0, 1, 2, 3, 4],
+    [1, 2, 3, 4, 4],
+    [2, 3, 4, 4, 4]
+  ];
+const M_overallRisk = [
+    [0, 1, 2, 3, 4],
+    [1, 1, 2, 3, 4],
+    [2, 2, 2, 3, 4],
+    [3, 3, 3, 3, 4],
+    [4, 4, 4, 4, 4]
+  ];
+//missing the Threat Event Frequency matrix, need from industry partner.
+
+module.exports = { M_PLEF };
+module.exports = { M_VULN };
+module.exports = { M_SLEF };
+module.exports = { primaryRisk };
+module.exports = { secondaryRisk };
+module.exports = { M_overallRisk };
--- server/server.js
+++ server/server.js
@@ -1,59 +1,29 @@
-const LEF = [
-    [3, 4, 5, 5, 5],
-    [2, 3, 4, 4, 4],
-    [1, 2, 3, 3, 3],
-    [1, 1, 2, 2, 2],
-    [1, 1, 1, 1, 1]
-  ];
-const vuln = [
-    [5, 5, 5, 4, 3],
-    [5, 5, 4, 3, 2],
-    [5, 4, 3, 2, 1],
-    [4, 3, 2, 1, 1],
-    [3, 2, 1, 1, 1]
-  ];
-const SLEF = [
-    [3, 4, 5, 5, 5],
-    [2, 3, 4, 5, 5],
-    [1, 2, 3, 4, 5],
-    [1, 1, 2, 3, 4],
-    [1, 1, 1, 2, 3]
-  ];
-const primaryRisk = [
-    [3, 4, 5, 5, 5],
-    [2, 3, 4, 5, 5],
-    [1, 2, 3, 4, 5],
-    [1, 1, 2, 3, 4],
-    [1, 1, 1, 2, 3]
-  ];
-const secondaryRisk = [
-    [3, 4, 5, 5, 5],
-    [2, 3, 4, 5, 5],
-    [1, 2, 3, 4, 5],
-    [1, 1, 2, 3, 4],
-    [1, 1, 1, 2, 3]
-  ];
-const overallRisk = [
-    [5, 5, 5, 5, 5],
-    [4, 4, 4, 4, 5],
-    [3, 3, 3, 4, 5],
-    [2, 2, 3, 4, 5],
-    [1, 2, 3, 4, 5]
-  ];
-//missing the Threat Event Frequency matrix, need from industry partner.
+const algo = require('./algo');
 
-var PAD = [0, 0];           //Probability of Action Deterrence
-var CFA = [0, 0];           //Contact Frequency Avoidance
-var RSV = [0, 0];           //Resistance Strength Vulnerability
-var SLMR = [0, 0];          //Secondary Loss Magnitude Responsive
-var TC = 0;                 //Threat Capability
-var SLP = 0;                //Secondary Loss Probability
+//get from frontend inputs
+var PAD = [0, 0];                                    //Probability of Action Deterrence
+var CFA = [0, 0];                                    //Contact Frequency Avoidance
+var RSV = [0, 0];                                    //Resistance Strength Vulnerability
+var PLMR = [0,0];                                    //Primary Loss Magnitude Responsive
+var SLMR = [0, 0];                                   //Secondary Loss Magnitude Responsive
+var TC = 0;                                          //Threat Capability
+var SLP = 0;                                         //Secondary Loss Probability
 
-const {MongoClient} = require('mongodb');
+var PADout = 1;
+var CFAout = 2;
+//var TEFout = M_TEF[CFAout,PADout];                 //maybe reverse order of inputs in array?
+var RSVout = 3;                                      //this needs to be changed to the output of RSV matrix
+//var VULNout = M_VULN[TC, RSVout];
+var LEFout = algo.M_PLEF[TEFout, VULNout];
+var PLMRout = 4;                                     //this needs to be changed to the output of PLMR matrix
+//var primaryRiskout = M_primaryRisk[PLMRout,LEFout];
+
+
+const {MongoClient, DBRef} = require('mongodb');
 
 async function main(){
   const url = "mongodb+srv://opportun16:nZJxFmLrhK5sWuZ@cluster0.qtrsx.mongodb.net/Cluster0?retryWrites=true&w=majority";
-  
+
   const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
   try{
