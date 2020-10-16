ba75b3036c7f8bbb5f11d918a8e60d4456ef0721
John <jortega43@ucmerced.edu>
2020-10-15 18:49:55 -0700

    OOP, fixed delete, update, select, and view

--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const algo = require('./algo');
+const Database = require('./Database');
 const {MongoClient, DBRef} = require('mongodb');
 // const {ObjectId} = require('mongodb');
 
@@ -27,35 +28,11 @@ async function main(){
   const url = "mongodb+srv://opportun16:nZJxFmLrhK5sWuZ@cluster0.qtrsx.mongodb.net/Cluster0?retryWrites=true&w=majority";
 
   const client = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
-  const prompt = require('prompt-sync')({sigint: true});
 
   try{
     await client.connect();
 
-    //await functionNumber(client, prompt);
-    var bool = false;
-    while(!bool){
-      let answer = prompt("Enter function operation: ");
-      input = Number(answer);
-    
-      if(input === 1){
-        await submitData(client, prompt);
-      }
-      else if(input === 2){
-        await findOneListingByName(client, prompt);
-      }
-      else if(input === 3){
-        await listDatabases(client);
-      }
-      else if(input === 4){
-        await deleteData(client, prompt);
-      }
-      else if(input === 5){
-        await update(client, prompt);
-      }
-      else
-        bool = true;
-    }
+    await Database.options(client);
 
   }
   catch(e){
@@ -68,44 +45,3 @@ async function main(){
 
 main().catch(console.error);
 
-async function listDatabases(client){
-  databasesList = await client.db().admin().listDatabases();
-
-  console.log("Databases");
-  databasesList.databases.forEach(db => console.log(` -${db.name}`));
-}
-
-async function submitData(client, prompt){ 
-  let newListing = {name: prompt("name: "), date: prompt("date: "), summary: prompt("summary: ")};
-  const result = await client.db("FAIR").collection("Data").insertOne(newListing);
-  console.log(`New listing created with the following id: ${result.insertedId}`);
-}
-
-async function findOneListingByName(client, prompt){
-  let nameList = prompt("Enter name of document: ");
-  result = await client.db("FAIR").collection("Data").findOne({name: nameList});
-
-  if (result){
-    console.log('Found a listing in the collection')
-    console.log(result);
-  }
-  else{
-    console.log("No results");
-  }
-}
-
-async function deleteData(client, prompt){
-  let idOfListing = prompt("Enter the id of the document you want deleted: ")
-
-  result = await client.db("FAIR").collection("Data").deleteOne({_id: idOfListing});
-  console.log('${result.deletedCount} document(s) was/were deleted.')
-}
-
-async function update(client, prompt) {
-  let section_Update = prompt("Enter name of listing: ");
-
-  result = await client.db("FAIR").collection("Data").updateOne({ name: section_Update }, { $set: {name: "John"} });
-
-  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
-  console.log(`${result.modifiedCount} document(s) was/were updated.`);
-}
