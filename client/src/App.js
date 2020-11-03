74fe31e724040b18e09b4db5feea614c84cc6757
tsills <tsills@ucmerced.com>
2020-11-03 15:37:07 -0800

    added 12 dropdowns and a button

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,8 @@
-<<<<<<< HEAD
-import React from 'react';
-import Navbar from "./components/Navbar/Navbar"; 
-import './App.css';
-
-function App() { //app
-  return (
-    <div className="App">
-      <Navbar />
-    </div>
-  );
-}
-
-export default App;
-=======
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import {Navbar} from './components/SimonNavBar'
+
 
 function App() {
   return (
@@ -27,4 +13,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 7346bb388e856e1efe4c95162f0f07ad4ddb6314
--- client/src/components/SimonNavBar/index.jsx
+++ client/src/components/SimonNavBar/index.jsx
@@ -1,11 +1,35 @@
 import React, {useState} from 'react'
 import Pagination from 'react-bootstrap/Pagination'
 import PageItem from 'react-bootstrap/PageItem'
+import Button from 'react-bootstrap/Button'
+import Dropdown from 'react-bootstrap/Dropdown'
+import DropdownButton from 'react-bootstrap/DropdownButton';
 //import page1 from asdfasdf
 //import page2 from asdfasdfasdf
 
 export function Navbar(props){
     const [active,setActive] = useState(1);
+    const [d1, setd1] = useState('');
+    const [d2, setd2] = useState('');
+    const [buttonDisabled, setButtonDisabled] = useState(true);
+    const [buttonDisabled2, setButtonDisabled2] = useState(true);
+
+  
+
+    const handleSelect=(e)=>{
+        console.log(e);
+        setd1(e);   
+        setButtonDisabled(false); 
+    }
+
+    const handleSelect2=(e)=>{
+        setd2(e);
+        setButtonDisabled2(false);
+    }
+
+    const handleClick=(e)=>{
+        alert("You chose " + d1 + " and " + d2);
+    }
 
     return(
         <React.Fragment>
@@ -19,7 +43,109 @@ export function Navbar(props){
             </Pagination>
         </div>
         <Pages >
-            <Page key={1} active={1 === active}>I am page 1</Page>
+            <Page key={1} active={1 === active}>
+
+                <Button  onClick={handleClick} disabled={buttonDisabled, buttonDisabled2} style={{backgroundColor: '#0B0C10', borderColor: '#45A293', color: '#45A293', borderRadius: '100px'}} >Submit</Button>{' '}
+
+                <DropdownButton title="values" id = "dropdown" onSelect={handleSelect}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton> 
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+                
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+                <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+                    <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                    <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                    <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                    <Dropdown.Item eventKey="5">3</Dropdown.Item>
+                </DropdownButton>  
+
+               <h4>You selected {d1} and {d2}</h4>
+
+            </Page>
             <Page key={2} active={2 === active}>I am page 2</Page>
             <Page key={3} active={3 === active}>I am page 3</Page>
             <Page key={4} active={4 === active}>I am page 4</Page>
