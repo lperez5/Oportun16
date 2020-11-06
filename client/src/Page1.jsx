1b9dff495895e97d525ba8b0634a2a2d40e97149
luisperezsplp@gmail.com <lperez53@ucmerced.edu>
2020-11-05 21:17:55 -0800

    page1

--- client/src/Page1.jsx
+++ client/src/Page1.jsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
@@ -102,24 +102,6 @@ export function Page1(props){
    </Row>
   </Container>
     </div>
+  
   );
-=======
-import React from 'react'
-import Dropdown from 'react-bootstrap/Dropdown'
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import ToggleButton from 'react-bootstrap/ToggleButton'
-
-
-export function Page1(props){
-  return(
-    <DropdownButton id="dropdown-item-button" title="Vuln">
-      <Dropdown.ItemText> </Dropdown.ItemText>
-      <Dropdown.Item as="button">771</Dropdown.Item>
-      <Dropdown.Item as="button">2</Dropdown.Item>
-      <Dropdown.Item as="button">3</Dropdown.Item>
-      <Dropdown.Item as="button">4</Dropdown.Item>
-      <Dropdown.Item as="button">5</Dropdown.Item>
-    </DropdownButton>
-  );
->>>>>>> 171eef8c39a204349be392a761d479069bef82c4
 }
