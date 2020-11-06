eab4d73b5cbfb32f00bb370d6c3008ef060a4cd0
luisperezsplp@gmail.com <lperez53@ucmerced.edu>
2020-11-05 20:55:48 -0800

    got page layout

--- client/src/Page1.jsx
+++ client/src/Page1.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
 import DropdownButton from 'react-bootstrap/DropdownButton'
 import ToggleButton from 'react-bootstrap/ToggleButton'
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Form from 'react-bootstrap/Form'
+import Card from 'react-bootstrap/Card'
+import { Button, Container, Row, Col } from 'react-bootstrap';
 
 export function Page1(props){
   const {active} = props;             //const active = props.active
@@ -16,6 +19,87 @@ export function Page1(props){
         <Dropdown.Item as="button">4</Dropdown.Item>
         <Dropdown.Item as="button">5</Dropdown.Item>
       </DropdownButton>
+
+      <Container>
+        <Row>
+          <Col>
+            <Form>
+              <Form.Group controlId="formBasicEmail">
+                <Form.Control type="email" placeholder="Search"/>
+              </Form.Group>
+            </Form>
+          </Col>
+          <Col>
+          <Form>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Control as="select">
+      <option>Date</option>
+      <option>Date</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group> 
+</Form>
+</Col>
+          <Col>
+          <Form>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Control as="select">
+      <option>Category</option>
+      <option>Date</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </Form.Control>
+  </Form.Group> 
+</Form>
+          </Col>
+        </Row>
+      </Container>
+
+      <Container>
+  <Row>
+    <Col> NAME </Col>
+    <Col>
+      <Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Text>
+        date
+        </Card.Text>
+      </Card.Body>
+      </Card>
+    </Col>
+     <Col>
+      <Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Text>
+        category
+        </Card.Text>
+      </Card.Body>
+      </Card>
+     </Col>
+      <Col>
+       <Card style={{ width: '10rem' }}>
+      <Card.Body>
+        <Card.Text>
+        data
+        </Card.Text>
+      </Card.Body>
+      </Card>
+      </Col>
+       <Col>
+         <Button variant="primary">
+            Options
+          </Button>
+        </Col>
+      <Col>
+          <Button variant="primary">
+            Details
+          </Button>
+      </Col>
+   </Row>
+  </Container>
     </div>
   );
 }
