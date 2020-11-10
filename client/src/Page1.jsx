471ae7e321a50d2c57c05d0b33d73cc67fbc2350
tsills <tsills@ucmerced.com>
2020-11-10 15:05:25 -0800

    readded and labeled 12 dropdowns

--- client/src/Page1.jsx
+++ client/src/Page1.jsx
@@ -9,31 +9,44 @@ export function Page1(props){
     const {active} = props; 
 
     
-    const [d1, setd1] = useState('');
-    const [d2, setd2] = useState('');
+    const [padI, setpadI] = useState('');
+    const [padR, setpadR] = useState('');
+    const [cfaI, setcfaI] = useState('');
+    const [cfaR, setcfaR] = useState('');
+    const [rsI, setrsI] = useState('');
+    const [rsR, setrsR] = useState('');
+    const [plmrI, setplmrI] = useState('');
+    const [plmrR, setplmrR] = useState('');
+    const [slmrI, setslmrI] = useState('');
+    const [slmrR, setslmrR] = useState('');
+    const [tc, settc] = useState('');
+    const [slp, setslp] = useState('');
+
     const [buttonDisabled, setButtonDisabled] = useState(true);
     const [buttonDisabled2, setButtonDisabled2] = useState(true);
 
-    const handleSelect=(e)=>{
-        console.log(e);
-        setd1(e);   
-        setButtonDisabled(false); 
-    }
-
-    const handleSelect2=(e)=>{
-        setd2(e);
-        setButtonDisabled2(false);
-    }
+    const handleSelectpadI=(e)=>{setpadI(e);setButtonDisabled(false);}
+    const handleSelectpadR=(e)=>{setpadR(e);setButtonDisabled2(false);}
+    const handleSelectcfaI=(e)=>{setcfaI(e);setButtonDisabled2(false);}
+    const handleSelectcfaR=(e)=>{setcfaR(e);setButtonDisabled2(false);}
+    const handleSelectrsI=(e)=>{setrsI(e);setButtonDisabled2(false);}
+    const handleSelectrsR=(e)=>{setrsR(e);setButtonDisabled2(false);}
+    const handleSelectplmrI=(e)=>{setplmrI(e);setButtonDisabled2(false);}
+    const handleSelectplmrR=(e)=>{setplmrR(e);setButtonDisabled2(false);}
+    const handleSelectslmrI=(e)=>{setslmrI(e);setButtonDisabled2(false);}
+    const handleSelectslmrR=(e)=>{setslmrR(e);setButtonDisabled2(false);}
+    const handleSelecttc=(e)=>{settc(e);setButtonDisabled2(false);}
+    const handleSelectslp=(e)=>{setslp(e);setButtonDisabled2(false);}
 
     const handleClick=(e)=>{
-        alert("You chose " + d1 + " and " + d2);
+        alert("You chose " + padI + " and " + padR);
     }
 
     return(
         <div hidden={!active}>
             <Button  onClick={handleClick} disabled={buttonDisabled, buttonDisabled2} style={{backgroundColor: '#0B0C10', borderColor: '#45A293', color: '#45A293', borderRadius: '100px'}} >Submit</Button>{' '}
-
-            <DropdownButton title={d1} id = "dropdown" onSelect={handleSelect}>
+           
+            <DropdownButton title={padI} id = "PADInherent" onSelect={handleSelectpadI}>
                 <Dropdown.Item eventKey="1">1</Dropdown.Item>
                 <Dropdown.Item eventKey="2">2</Dropdown.Item>
                 <Dropdown.Item eventKey="3">3</Dropdown.Item>
@@ -41,7 +54,7 @@ export function Page1(props){
                 <Dropdown.Item eventKey="5">3</Dropdown.Item>
             </DropdownButton>  
 
-            <DropdownButton title="values2" id = "dropdown2" onSelect={handleSelect2}>
+            <DropdownButton title={padR} id = "PADResidual" onSelect={handleSelectpadR}>
                 <Dropdown.Item eventKey="1">1</Dropdown.Item>
                 <Dropdown.Item eventKey="2">2</Dropdown.Item>
                 <Dropdown.Item eventKey="3">3</Dropdown.Item>
@@ -49,7 +62,87 @@ export function Page1(props){
                 <Dropdown.Item eventKey="5">3</Dropdown.Item>
             </DropdownButton> 
 
-            <h4>You selected {d1} and {d2}</h4>
+            <DropdownButton title={cfaI} id = "CFAInherent" onSelect={handleSelectcfaI}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={cfaR} id = "CFAResidual" onSelect={handleSelectcfaR}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={rsI} id = "RSInherent" onSelect={handleSelectrsI}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={rsR} id = "RSResidual" onSelect={handleSelectrsR}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={plmrI} id = "PLMRInherent" onSelect={handleSelectplmrI}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={plmrR} id = "PLMRResidual" onSelect={handleSelectplmrR}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={slmrI} id = "SLMRInherent" onSelect={handleSelectslmrI}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={slmrR} id = "SLMRResidual" onSelect={handleSelectslmrR}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={tc} id = "TC" onSelect={handleSelecttc}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <DropdownButton title={slp} id = "SLP" onSelect={handleSelectslp}>
+                <Dropdown.Item eventKey="1">1</Dropdown.Item>
+                <Dropdown.Item eventKey="2">2</Dropdown.Item>
+                <Dropdown.Item eventKey="3">3</Dropdown.Item>
+                <Dropdown.Item eventKey="4">3</Dropdown.Item>
+                <Dropdown.Item eventKey="5">3</Dropdown.Item>
+            </DropdownButton> 
+
+            <h4>You selected {padI} and {padR}</h4>
 
         </div>
     );
