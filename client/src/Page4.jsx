f26ffb22e4754f73ba33306fbd9065d0452ef6f1
luisperezsplp@gmail.com <lperez53@ucmerced.edu>
2020-11-13 21:37:38 -0800

    page4 with tree and mehtodlgy are in place, little choppy

--- client/src/Page4.jsx
+++ client/src/Page4.jsx
@@ -6,6 +6,9 @@ import DropdownButton from 'react-bootstrap/DropdownButton';
 import Form from 'react-bootstrap/Form'
 import Card from 'react-bootstrap/Card'
 import {  Container, Row, Col } from 'react-bootstrap';
+import Tree from '../src/tree.png'
+import Methodology1 from '../src/methodologyp1.png'
+import Methodology2 from '../src/methodologyp2.png'
 
 export function Page4(props){
     const {active} = props;
@@ -29,10 +32,13 @@ export function Page4(props){
 
             <Container fluid="md">
                 <Row>
-                    <Col>TREE</Col>
+                    <Col><img src={Tree} /></Col>
                 </Row>
                 <Row>
-                    <Col>Method Table</Col>
+                    <Col><img src={Methodology1} /></Col>
+                </Row>
+                <Row>
+                    <Col><img src={Methodology2} /></Col>
                 </Row>
             </Container>
 
