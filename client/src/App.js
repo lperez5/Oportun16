fd4231556e796624a6a1840ae477c2c6d6c98af5
jortega99 <johnortega99@gmail.com>
2020-11-30 13:19:00 -0800

    DB loads entries click

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React, {useState} from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import axios from 'axios'
 import {Navbar} from './components/Navbar/index.jsx'
-import {handleCache} from './components/Navbar/index.jsx'
 
+var DBarray = [];
 class App extends React.Component {
 
   state = {
@@ -16,46 +15,21 @@ class App extends React.Component {
   async componentDidMount() {
     const URL = '/api';
     const response = await fetch(URL);
-    const data = await response.json();
-    this.setState({posts: data});
-    //console.log(data[0])
-    handleCache(data)
+    DBarray = await response.json();
+    this.setState({posts: DBarray});
+    //console.log(DBarray[0])
     console.log('Data received')
   };
-  // getBlogPost = () => {
-  //   axios.get('/api')
-  //     .then((response) => {
-  //       const data = response.data;
-  //       //this.setState({ posts: data });
-  //       handleCache(data[0]);
-  //       console.log(data[0])
-  //       console.log('Data has been received!!');
-  //     })
-  //     .catch(() => {
-  //       alert('Error retrieving data!!!');
-  //     });
-  // }
-
   render() {
 
     console.log('State: ', this.state);
-
     //JSX
     return(
           <div className="App">
-            <Navbar />
+            <Navbar dbArray = {DBarray} />
           </div>
     );
   }
 }
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <Navbar />
-//     </div>
-//   );
-// }
 
 export default App;
