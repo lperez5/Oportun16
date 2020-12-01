24402450f54bdcdf3716e64489821c0b3ce0554b
Burritoj <matsimoncode@gmail.com>
2020-11-30 21:31:46 -0800

    dont need setarray in pg3

--- client/src/Page3.jsx
+++ client/src/Page3.jsx
@@ -4,7 +4,7 @@ import Button from 'react-bootstrap/Button'
 
 export function Page3(props){
 
-  const {active, dbCacheArray, setdbCacheArray} = props;
+  const {active, dbCacheArray} = props;
   let numVeryHighInherentOR = 0;
 
   for(let j=0; j<getTotalDocs(dbCacheArray); j++){
