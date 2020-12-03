2242440470bde451ea37afb83dc8542c8f695e92
Burritoj <matsimoncode@gmail.com>
2020-12-02 23:13:19 -0800

    merging everything for lab 4.4

--- server/models/entry.js
+++ server/models/entry.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const mongoose = require('mongoose');
 
 
@@ -8,7 +7,41 @@ const entry = new Schema({
   category: String,
   data: Array,
   notes: String,
-  asset: String,
+  asset: String,function monthlyChange(arr, x){
+  let thisMonthAvg = 0;
+  let lastMonthAvg = 0;
+  let thisMonth = new Date().getMonth();
+  let thisYear = new Date().getFullYear();
+  let counter1 = 0;
+  let counter2 = 0;
+
+  for(let i=0; i < arr.length; i++){
+    if(getYear(arr[i].dateCreated) === thisYear && getThisMonth(arr[i].dateCreated) === thisMonth){
+      counter1++;
+      thisMonthAvg += arr[i].data[x];
+    }
+    if(getYear(arr[i].dateCreated) === thisYear && getThisMonth(arr[i].dateCreated) === (thisMonth-1)){
+      counter2++;
+      lastMonthAvg += arr[i].data[x];
+    }
+  }
+
+  if(counter1 === 0){
+    thisMonthAvg = 0;
+  }
+  else{
+    thisMonthAvg = thisMonthAvg/counter1;
+  }
+
+  if(counter2 === 0){
+    lastMonthAvg = 0;
+  }
+  else{
+    lastMonthAvg = lastMonthAvg/counter2;
+  }
+
+  return (thisMonthAvg - lastMonthAvg);
+}
   threat: String,
   loss: String,
   dateCreated: {type: String, default: new Date(Date.now())},
@@ -18,25 +51,4 @@ const entry = new Schema({
 //Model
 const entrymodel = mongoose.model('Entry', entry);
 
-=======
-const mongoose = require('mongoose');
-
-
-const Schema = mongoose.Schema;
-const entry = new Schema({
-  name: String,
-  category: String,
-  data: Array,
-  notes: String,
-  asset: String,
-  threat: String,
-  loss: String,
-  dateCreated: {type: String, default: new Intl.DateTimeFormat('en-US').format(Date.now())},
-  lastUpdated: {type: String, default: new Intl.DateTimeFormat('en-US').format(Date.now())},
-});
-
-//Model
-const entrymodel = mongoose.model('Entry', entry);
-
->>>>>>> f44dff9ad040f878f7ae2fd570b235eb2b5e5141
 module.exports = entrymodel;
